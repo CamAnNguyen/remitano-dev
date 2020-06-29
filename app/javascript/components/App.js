2c3d1d4660c5f41c88d88003eb3a3c4681745c59
an.nguyen <an.nguyen@kit.edu>
2020-06-29 10:03:52 +0200

    Init react-rails project

---
+++ app/javascript/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Greeting: {this.props.greeting}
+      </React.Fragment>
+    );
+  }
+}
+
+App.propTypes = {
+  greeting: PropTypes.string
+};
+
+export default App;
