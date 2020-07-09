e3c9eea34627c29774fe3c1b39f52d1a2c7bd767
an.nguyen <an.nguyen@kit.edu>
2020-07-09 12:12:20 +0200

    Code styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@
 # Main application controller
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :null_session, only: proc { |c| c.request.format.json? }
+
+  protect_from_forgery with: :null_session,
+                       only: proc { |c| c.request.format.json? }
 end
