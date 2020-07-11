85a215c2c3468c09030e58c34809e5878e66fc04
an.nguyen <an.nguyen@kit.edu>
2020-07-11 12:47:51 +0200

    Add explanation comment for redux-auth-token config usage

--- app/javascript/actions/authTokenActions.js
+++ app/javascript/actions/authTokenActions.js
@@ -4,7 +4,9 @@ import { AUTH_URL } from '../constants/url';
 
 const config = {
   authUrl: AUTH_URL,
-  // contains attributes from User model,
+  // Contains attributes from User model
+  // With below config, only email attributes would be displayed
+  // NOTE: null or empty value would cause undefined value in reducer
   userAttributes: { email: 'email' },
   // contains attributes that necessary for registration
   userRegistrationAttributes: {},
