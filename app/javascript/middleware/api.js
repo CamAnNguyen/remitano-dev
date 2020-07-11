df0f464e58c14cc1a895ba17c86f17819dfe9292
an.nguyen <an.nguyen@kit.edu>
2020-07-11 10:57:40 +0200

    Change api.js to use axios

--- app/javascript/middleware/api.js
+++ app/javascript/middleware/api.js
@@ -1,37 +1,26 @@
-import 'whatwg-fetch';
-
-import { camelizeKeys } from 'humps';
+import axios from 'axios';
 
 export const CALL_API = 'Call API';
 
-const callApi = (endpoint, options = {}) => (
-  fetch(endpoint, options).then(response => (
-    response.json().then((json) => {
-      if (!response.ok) {
-        return Promise.reject(json);
-      }
+const callApi = ({ method, url, data }) => axios({ method, url, data });
 
-      return camelizeKeys(json);
-    })
-  ))
-);
-
-export default store => next => (action) => {
+export default (store) => (next) => (action) => {
   const api = action[CALL_API];
   if (typeof api === 'undefined') {
     return next(action);
   }
 
-  const { endpoint, options } = api;
+  const { method, url, data } = api;
   let { normalizer } = api;
-  if (typeof endpoint !== 'string') {
+  if (typeof url !== 'string') {
     throw new Error('Specify a string endpoint URL.');
   }
+
   if (!normalizer) {
-    normalizer = res => res;
+    normalizer = (res) => res;
   }
 
-  return callApi(endpoint, options).then(response => next({
+  return callApi({ method, url, data }).then((response) => next({
     response: normalizer(response, store, action.type),
     ...action
   }));
