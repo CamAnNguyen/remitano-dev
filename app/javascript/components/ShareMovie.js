ca415064f1109368f3de8f93a6b184fea85d949f
an.nguyen <an.nguyen@kit.edu>
2020-07-12 11:41:41 +0200

    Update ShareMovie UI component

--- app/javascript/components/ShareMovie.js
+++ app/javascript/components/ShareMovie.js
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
-import { Col } from 'react-bootstrap';
+import {
+  Button, Col, Form, FormControl, Row
+} from 'react-bootstrap';
 
 function ShareMovie({ shareMovie }) {
+  const urlRef = useRef(null);
+  const shareFunc = useCallback(() => shareMovie(urlRef.current.value), []);
+
   return (
-    <>
-      SHARE MOVIE
-    </>
+    <div id="share-movie-block">
+      <div id="share-movie-title">
+        <b>Share a Youtube movie</b>
+      </div>
+      <div id="share-movie-content">
+        <Form>
+          <Form.Group as={Row} style={{ marginBottom: '0px' }}>
+            <Form.Label column sm={3} style={{ paddingLeft: '25px' }}>
+              Youtube URL:
+            </Form.Label>
+            <Col>
+              <FormControl
+                id="shareMovieFormYoutubeURL"
+                ref={urlRef}
+                size="sm"
+                placeholder="Youtube URL"
+                className="mb-2 mr-sm-2"
+                style={{ marginTop: '5px' }}
+              />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} style={{ marginBottom: '0px' }}>
+            <Form.Label column sm={3} />
+            <Col>
+              <Button
+                variant="secondary"
+                size="sm"
+                className="mb-2"
+                onClick={shareFunc}
+              >
+                Share
+              </Button>
+            </Col>
+          </Form.Group>
+        </Form>
+      </div>
+    </div>
   );
 }
 
