91810ec480277de58fa94f781ad5eab340b29205
an.nguyen <an.nguyen@kit.edu>
2020-07-12 17:12:29 +0200

    Remove commented code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,4 @@
 # Main React App controller
 class HomeController < ApplicationController
   respond_to :json
-
-  # def authenticate!
-  #   error!('401 Unauthorized', 401) unless current_user
-  # end
 end
